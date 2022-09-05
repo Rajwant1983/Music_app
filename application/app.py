3129be48552472ea654e7899a0caa0a35a14291f
Rajwant <rajwantkaurthethi1983@gmail.com>
2022-09-05 19:04:57 +0100

    changes

--- application/app.py
+++ application/app.py
@@ -12,7 +12,7 @@ d = SQLAlchemy(application)
 from application.routes import *
 
 if __name__ == '__main__':
-    application.run(debug=True,host='0.0.0.0',port='8086')
+    application.run(debug=True,host='0.0.0.0',port='8085')
 
 
 # Run web server using this file
